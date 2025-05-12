45a4a790a951177c5dcdbabb507fe99f96f2acae
Mirlan2121 <askarbekovmirlan40@gmail.com>
2025-05-12 16:03:40 +0900

    Java Core 1-20

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //В Java типы данных делят на две большие группы: примитивные и ссылочные.
+        // В состав примитивных типов (или просто примитивов) входят
+        // четыре подвида и восемь типов данных:
+
+        //Целые числа Java (byte, short, int, long);
+        int num = 123456789;
+        long numL = 1234567890;
+        byte numB = 123;
+        short numS = 12345;
+
+        //Числа с плавающей точкой (float, double);
+        double num1 = 1.23456789;
+        float num2= 1.234567f;
+
+        //Логический (boolean);
+        boolean bool = true;
+        boolean bool1 = false;
+
+        //Символьный (char).
+        char ch = 'c';
+
+        System.out.println();
+    }
+}
---
+++ src/Num.java
@@ -0,0 +1,6 @@
+public class Num {
+    public static void main(String[] args){
+        int num = 1000;
+        System.out.println(num);
+    }
+}
